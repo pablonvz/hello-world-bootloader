01151c2f74077f6211d375d06ab8001df7338834
Pablo Castelnovo <pablonvz@gmail.com>
2014-05-07 19:51:26 -0400

    Adding software used and its versions to requirements. Adding comments to asm code

--- bootloader.asm
+++ bootloader.asm
@@ -1,17 +1,16 @@
-
-; source: http://viralpatel.net/taj/tutorial/hello_world_bootloader.php
+; source: http://www.osdever.net/tutorials/view/hello-world-boot-loader
 
 		[BITS 16]
 		[ORG 0x7c00]
 
 		mov si, hw_str
-		mov ah, 0x0e
+		mov ah, 0x0e	; function (0x0e) puts a character on the screen
 		mov bh, 0x00
 		mov bl, 0x07
 		
 		call pstrf
 
-pstrf:		mov al, [si]
+pstrf:		mov al, [si]	; pstrf -> print a character while not equal to zero
 		cmp al, 0
 		jz done
 		int 0x10
@@ -22,6 +21,6 @@ done:		nop
 
 hw_str db 'Hello World!', 0
 
-times 510 - ($ - $$) db 0
-dw 0xaa55
+times 510 - ($ - $$) db 0	; fill the rest of the sector with zero's
+dw 0xaa55			; add the bootloader signature at the end
 
